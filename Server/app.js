741f14e2ab91aeabe4fa314515de2f45c4aab2e3
NetanelCohen241 <netanel2411@gmail.com>
2019-05-20 16:06:20 +0300

    initial commit

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,9 @@ app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
 
-app.get('/select', function(req, res){
+
+
+app.post('/login', function(req, res){
     DButilsAzure.execQuery("SELECT * FROM tableName")
         .then(function(result){
             res.send(result)
@@ -17,3 +19,169 @@ app.get('/select', function(req, res){
             res.send(err)
         })
 })
+
+
+app.post('/register', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+app.post('/getSecurityQuestion', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+app.post('/restorePassword', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+app.get('/getAllPOI', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+app.post('/getUserFavoriteFields', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+app.post('/getUserFavoritePOI', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+
+app.get('/getAllFields', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+app.post('/saveFavoraitePOI', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+app.post('/getPOIbyID', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+app.post('/addRating', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+
+app.post('/addReview', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+
+app.post('/saveFavoraitePOIOrder', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM tableName")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+
+
+
+
+
+
